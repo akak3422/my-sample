e84fabf02e99bf7a9dec6cb4fd5c553ebddb6890
akak3422 <95001530+akak3422@users.noreply.github.com>
2021-11-24 15:53:56 -0800

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  createCanvas(400,400);
+  var box=createSprite(200,200,20,20);
+box.shapeColor = "pink";
+}
+
+function draw() 
+{
+  background("yellow");
+drawSprites ();
+}
+
+
+
+
